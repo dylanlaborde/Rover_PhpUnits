b4130646262ffbcfc6ed4330d90c5586f74661a6
Dylan <dylan4047@gmail.com>
2017-01-27 15:18:04 +0100

    testing aour function that maked the rover to turn left and right

--- RoverTest.php
+++ RoverTest.php
@@ -4,7 +4,7 @@ use PHPUnit\Framework\TestCase;
 class TestRover extends TestCase{
 
 	
-
+//Default
 	public function testDefaultPosition(){
 		$rover = new Rover();
 		$this->assertEquals(
@@ -15,6 +15,7 @@ class TestRover extends TestCase{
 			"direction"=>"north"]);
 
 	}
+	// Multiple Command
 	public function testExecuteCommand(){
 		$rover2= new Rover();
 		$rover2->ExecuteCommand(["f","f"]);
@@ -25,6 +26,8 @@ class TestRover extends TestCase{
 			"y"=>2,
 			"direction"=>"north"]);
 	}
+
+	// Forward Function
 	public function testmoveRoverForwardIfNorth(){
 		$rover3= new Rover();
 		
@@ -40,25 +43,9 @@ class TestRover extends TestCase{
 
 
 	}
-	public function testmoveRoverBackwardIfNorth(){
-		$rover3= new Rover();
-		
-		$rover3->MoveRover("b");
-		$this->assertEquals(
-
-			$rover3->getCoordinate(),
-			[
-			"x"=>0,
-			"y"=>-1,
-			"direction"=>"north"]);
-
-
-
-	}
 	public function testmoveRoverForwardIfSouth(){
 		$rover4 =new Rover();
-		// $rover4->ExecuteCommand("f");
-		 $rover4->ExecuteCommand(["r","r","f"]);
+		$rover4->ExecuteCommand(["r","r","f"]);
 		$this->assertEquals(
 			$rover4->getCoordinate(),
 			["x"=>0,
@@ -68,31 +55,84 @@ class TestRover extends TestCase{
 
 
 	}
-	public function testmoveRoverBackwardIfSouth(){
-		$rover6= new Rover();
-		$rover6->ExecuteCommand(["r","r","b"]);
+	public function testmoveRoverForwardIfEst(){
+		$rover7 = new Rover();
+		$rover7->ExecuteCommand(["r","f"]);
 		$this->assertEquals(
-			$rover6->getCoordinate(),
-			["x"=>0,
-			"y"=>-1,
-			"direction"=>"south"]
-			);
-	}
-	
-
-
-	public function testTurnRover(){
-
-		$rover5 =new Rover();
-		$rover5->TurnRover("r");
-		$this->assertEquals(
-			$rover5->getCoordinate(),
-			[
-			"x"=>0,
+			$rover7->getCoordinate(),
+			["x"=>1,
 			"y"=>0,
-			"direction"=>"east"]);
-			
-	}
+			"direction"=>"east"]
+			);		
+
+}
+public function testmoveRoverForwardIfOuest(){
+	$rover8 = new Rover();
+		$rover8->ExecuteCommand(["l","f"]);
+		$this->assertEquals(
+			$rover8->getCoordinate(),
+			["x"=>-1,
+			"y"=>0,
+			"direction"=>"ouest"]
+			);		
+
+
+}
+public function testmoveRoverForwardIfEast(){
+	$rover9 = new Rover();
+		$rover9->ExecuteCommand(["r","f"]);
+		$this->assertEquals(
+			$rover9->getCoordinate(),
+			["x"=>1,
+			"y"=>0,
+			"direction"=>"east"]
+			);		
+
+
+}
+	// Backward Function
+
+public function testmoveRoverBackwardIfNorth(){
+	$rover3= new Rover();
+
+	$rover3->MoveRover("b");
+	$this->assertEquals(
+
+		$rover3->getCoordinate(),
+		[
+		"x"=>0,
+		"y"=>-1,
+		"direction"=>"north"]);
+
+
+
+}
+public function testmoveRoverBackwardIfSouth(){
+	$rover6= new Rover();
+	$rover6->ExecuteCommand(["r","r","b"]);
+	$this->assertEquals(
+		$rover6->getCoordinate(),
+		["x"=>0,
+		"y"=>-1,
+		"direction"=>"south"]
+		);
+}
+
+
+
+// Turn 
+public function testTurnRover(){
+
+	$rover5 =new Rover();
+	$rover5->TurnRover("r");
+	$this->assertEquals(
+		$rover5->getCoordinate(),
+		[
+		"x"=>0,
+		"y"=>0,
+		"direction"=>"east"]);
+
+}
 
 }
 
