48b1ae092764d3221418261d4a1b0c3299b1cc81
Dylan <dylan4047@gmail.com>
2017-01-27 10:12:45 +0100

    ajout class Rover + coordonate + getter

--- Rover.php
+++ Rover.php
@@ -1,7 +1,20 @@
 <?php 
 
+class Rover{
+
+	private $coordinate = [
+	"x"=>0,
+	"y"=>0,
+	"direction"=>"north"];
+
+	public function getCoordinate(){
+		return $this->coordinate;
+	}
+
+
+}
 
 
 
 
- ?>
+?>
