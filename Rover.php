3b88cf695263804451ec7dbed696208127158d0f
Dylan <dylan4047@gmail.com>
2017-01-27 11:40:20 +0100

    ajout methode moveRover et executeCommand

--- Rover.php
+++ Rover.php
@@ -10,11 +10,42 @@ class Rover{
 	public function getCoordinate(){
 		return $this->coordinate;
 	}
+	public function ExecuteCommand($ArrayCommand){
+		$ArrayCommandlength = count($ArrayCommand);
+		for ($i=0; $i < $ArrayCommandlength ; $i++) { 
+			if ($ArrayCommand === "f") {
+				$this->MoveRover($ArrayCommand[$i]);// $this->MoveRover("f");
+			}else{
+				$this->coordinate["y"]++;
+			}
+			// if ($ArrayCommand === "b") {
+			// 	$this->MoveRover($ArrayCommand[$i]);
+			// }
+		}
+
+	}
+	public function MoveRover($command){
+		$direction = $this->coordinate["direction"];
+		if ($direction === "north") {
+			$this->moveRoverForwardIfNorth();
+		}
+		// if ($direction === "south") {
+		// 	$this->moveRoverForwardIfSouth();
+		// }
+		// if ($direction === "east") {
+		// 	$this->moveRoverForwardIfEst();
+		// }
+		// if ($direction === "ouest") {
+		// 	$this->moveRoverForwardIfOuest();
+		// }
+
+	}
+
+	public function moveRoverForwardIfNorth(){
+		$this->coordinate["y"]++;
+		
+	}
 
 
 }
-
-
-
-
 ?>
